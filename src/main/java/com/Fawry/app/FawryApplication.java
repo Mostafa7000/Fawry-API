0f662e8b657ab83606eb200f2b965440d955e1b7
Mostafa Tarek <mostafatarek7000@gmail.com>
2022-12-26 19:53:52 +0200

    Added project skeleton

---
+++ src/main/java/com/Fawry/app/FawryApplication.java
@@ -0,0 +1,13 @@
+package com.Fawry.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FawryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FawryApplication.class, args);
+	}
+
+}
